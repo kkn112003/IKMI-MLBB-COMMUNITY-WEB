1c5d77e7483d24b6129f4a61bcde12edb68fd0e0
kknkrnwn11 <126553398+kkn112003@users.noreply.github.com>
2025-07-06 00:41:39 +0700

    loading.js

---
+++ css/loading.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+  const loader = document.getElementById('loader-wrapper');
+  // Tambahkan kelas 'loader-hidden' untuk memulai transisi fade-out
+  loader.classList.add('loader-hidden');
+});
